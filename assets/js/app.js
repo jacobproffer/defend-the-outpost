b0d5a0c883fcbca47976b7da67fe05b7100ac4c8
jacobproffer <jacobhardingproffer@gmail.com>
2017-06-30 12:09:19 -0400

    Touch scroll test

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,9 +59,9 @@ hamburger.click(function() {
       if (!$(e.target).parents().hasClass('nav-open')) {
         e.preventDefault();
       }
-      if ( navigator.userAgent.match(/Android/i)) {
-        e.preventDefault();
-      }
+    });
+    body.addEventListener('touchstart', function(e) {
+      e.preventDefault();
     });
   } else {
     body.unbind('touchmove');
